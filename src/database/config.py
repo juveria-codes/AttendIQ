6a9b7201ab984e9445a018f3ecc780ae354b9adb
juveria-codes <juveria2301@gmail.com>
2026-09-04 13:08:39 +0530

    new changes to database

--- src/database/config.py
+++ src/database/config.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 from supabase import create_client, Client
 
-supabase : Client =(
-    st.secrets['SUPABASE_URL'],
-    st.secrets['SUPABASE_KEY']
+supabase : Client =create_client(
+    st.secrets["SUPABASE_URL"],
+    st.secrets["SUPABASE_KEY"]
 )
--- src/database/db.py
+++ src/database/db.py
@@ -17,9 +17,9 @@ def create_teacher(username, password, name):
     return response.data
 
 def teacher_login(username, password):
-    response = supabase.table('teachers').select('username').eq("username",username).execute()
+    response = supabase.table('teachers').select('*').eq("username",username).execute()
     if response.data:
         teacher = response.data[0]
-        if check_pass(password, teacher['password']):
-            return True
+        if check_pass(password, teacher["password"]):
+            return teacher
     return None
