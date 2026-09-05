28e6333b240e8cba84ccdc62ebdccb6688db9bef
juveria-codes <juveria2301@gmail.com>
2026-09-05 21:27:19 +0530

    student screen updated

--- src/screens/student_screen.py
+++ src/screens/student_screen.py
@@ -1,4 +1,29 @@
 import streamlit as st
 
+from src.ui.base_layout import style_background_dashboard, style_base_layout
+from src.components.header import header_dashboard
+from PIL import Image
+import numpy as np
+
 def student_screen():
-    st.header('Student Screen')
+
+
+    style_background_dashboard()
+    style_base_layout()
+
+    c1, c2 = st.columns(2, vertical_alignment="center", gap="xxlarge")
+        
+    with c1:
+        header_dashboard()
+    with c2:
+        if st.button("Go Back To Home", type="secondary", key = "loginbackbtn1", shortcut="control+backspace"):
+           st.session_state['login_type'] = None
+           st.rerun()
+
+    st.header('Login using FaceID', text_alignment='center')
+    st.space()
+
+    photo_source=st.camera_input("Position your face in the middle")
+
+    if photo_source:
+        np.array(Image.open((photo_source)))
